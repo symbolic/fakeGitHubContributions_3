a18b78d6224b4baf245817907a900696d8b48d9b
symbolic <vr@riseup.net>
2015-11-29 13:00:09 +0800

    commit

---
+++ locatTime.py
@@ -0,0 +1,19 @@
+#-*- coding:utf-8 -*- 
+
+import time 
+import random
+import os
+ 
+if __name__ == '__main__': 
+	timeSpaces = 5
+	
+	while True:
+		fp = open('random.txt', 'w')
+		fp.write(str(random.randint(0, 20)))
+		fp.close()
+		os.system('git add .')
+		os.system('git commit -m "commit"')
+		
+		time.sleep(timeSpaces)
+		
+	print 'finished'
